c3ab411177747f270d4c70510152b0cd9a59da30
jesusbeb <jbeltranb@outlook.com>
2025-06-18 17:37:43 -0600

    Conexion a APIs con Java para mostrar imagenes aleatorias

--- src/main/java/com/mycompany/gatos_app/GatosService.java
+++ src/main/java/com/mycompany/gatos_app/GatosService.java
@@ -1,9 +1,19 @@
 package com.mycompany.gatos_app;
 
+import java.io.IOException;
+
+
 public class GatosService {
     
-    public static void verGatos(){
-        
+    public static void verGatos() throws IOException{
+        //1. Traemos los datos de la API
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.thecatapi.com/v1/images/search?")
+                .get().build();
+
+        Response response = client.newCall(request).execute();
     }
     
 }
